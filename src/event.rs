222a1bfe32f4f6561f568368fe22b29180dccd57
Milinkov Nikita <milinkov.nik@gmail.com>
2024-05-01 12:51:07 +0300

    refactor user input handling

---
+++ src/event.rs
@@ -0,0 +1,41 @@
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
+
+#[derive(Debug)]
+pub enum Key {
+    ArrowUp,
+    ArrowDown,
+    ArrowLeft,
+    ArrowRight,
+    Escape,
+}
+
+pub fn read() -> std::io::Result<Option<Key>> {
+    match crossterm::event::read()? {
+        Event::Key(KeyEvent {
+            code: KeyCode::Up,
+            kind: KeyEventKind::Press,
+            ..
+        }) => Ok(Some(Key::ArrowUp)),
+        Event::Key(KeyEvent {
+            code: KeyCode::Down,
+            kind: KeyEventKind::Press,
+            ..
+        }) => Ok(Some(Key::ArrowDown)),
+        Event::Key(KeyEvent {
+            code: KeyCode::Left,
+            kind: KeyEventKind::Press,
+            ..
+        }) => Ok(Some(Key::ArrowLeft)),
+        Event::Key(KeyEvent {
+            code: KeyCode::Right,
+            kind: KeyEventKind::Press,
+            ..
+        }) => Ok(Some(Key::ArrowRight)),
+        Event::Key(KeyEvent {
+            code: KeyCode::Esc,
+            kind: KeyEventKind::Press,
+            ..
+        }) => Ok(Some(Key::Escape)),
+        _ => Ok(None),
+    }
+}
