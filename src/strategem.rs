063d3101903b1a0034e9e40c4dd20b1b5aa5108e
Milinkov Nikita <milinkov.nik@gmail.com>
2024-05-03 12:03:08 +0300

    add more strategems

--- src/strategem.rs
+++ src/strategem.rs
@@ -171,7 +171,23 @@ impl StrategemBuilder {
     }
 }
 
-const ALL_STRATEGEMS: [Strategem; 40] = [
+const ALL_STRATEGEMS: [Strategem; 56] = [
+    mg43_machine_gun(),
+    apw1_antimateriel_rifle(),
+    m105_stalwart(),
+    eat17_expendable_antitank(),
+    gr8_recoilless_rifle(),
+    flam40_flamethrower(),
+    ac8_autocannon(),
+    mg206_heavy_machine_gun(),
+    rs422_railgun(),
+    faf14_spear_launcher(),
+    gl21_grenade_launcher(),
+    las98_laser_cannon(),
+    arc3_arc_thrower(),
+    las99_quasar_cannon(),
+    rl77_airburst_rocket_launcher(),
+    exo45_patriot_exosuit(),
     reinforce(),
     sos_beacon(),
     resupply(),
@@ -213,6 +229,172 @@ const ALL_STRATEGEMS: [Strategem; 40] = [
     eagle_110mm_rocket_pods(),
     eagle_500kg_bomb(),
 ];
+
+pub const fn mg43_machine_gun() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .down()
+        .up()
+        .right()
+        .build("MG-43 Machine Gun")
+}
+
+pub const fn apw1_antimateriel_rifle() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .right()
+        .up()
+        .down()
+        .build("APW-1 Anti-Materiel Rifle")
+}
+
+pub const fn m105_stalwart() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .down()
+        .up()
+        .up()
+        .left()
+        .build("M-105 Stalwart")
+}
+
+pub const fn eat17_expendable_antitank() -> Strategem {
+    Strategem::builder()
+        .down()
+        .down()
+        .left()
+        .up()
+        .right()
+        .build("EAT-17 Expendable Anti-tank")
+}
+
+pub const fn gr8_recoilless_rifle() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .right()
+        .right()
+        .left()
+        .build("GR-8 Recoilless Rifle")
+}
+
+pub const fn flam40_flamethrower() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .up()
+        .down()
+        .up()
+        .build("FLAM-40 Flamethrower")
+}
+
+pub const fn ac8_autocannon() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .down()
+        .up()
+        .up()
+        .right()
+        .build("AC-8 Autocannon")
+}
+
+pub const fn mg206_heavy_machine_gun() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .up()
+        .down()
+        .down()
+        .build("MG-206 Heavy Machine Gun")
+}
+
+pub const fn rs422_railgun() -> Strategem {
+    Strategem::builder()
+        .down()
+        .right()
+        .down()
+        .up()
+        .left()
+        .right()
+        .build("RS-422 Railgun")
+}
+
+pub const fn faf14_spear_launcher() -> Strategem {
+    Strategem::builder()
+        .down()
+        .down()
+        .up()
+        .down()
+        .down()
+        .build("FAF-14 SPEAR Launcher")
+}
+
+pub const fn gl21_grenade_launcher() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .up()
+        .left()
+        .down()
+        .build("GL-21 Grenade Launcher")
+}
+
+pub const fn las98_laser_cannon() -> Strategem {
+    Strategem::builder()
+        .down()
+        .left()
+        .down()
+        .up()
+        .left()
+        .build("LAS-98 Laser Cannon")
+}
+
+pub const fn arc3_arc_thrower() -> Strategem {
+    Strategem::builder()
+        .down()
+        .right()
+        .down()
+        .up()
+        .left()
+        .left()
+        .build("ARC-3 Arc Thrower")
+}
+
+pub const fn las99_quasar_cannon() -> Strategem {
+    Strategem::builder()
+        .down()
+        .down()
+        .up()
+        .left()
+        .right()
+        .build("LAS-99 Quasar Cannon")
+}
+
+pub const fn rl77_airburst_rocket_launcher() -> Strategem {
+    Strategem::builder()
+        .down()
+        .up()
+        .up()
+        .left()
+        .right()
+        .build("RL-77 Airburst Rocket Launcher")
+}
+
+pub const fn exo45_patriot_exosuit() -> Strategem {
+    Strategem::builder()
+        .left()
+        .down()
+        .right()
+        .up()
+        .left()
+        .down()
+        .down()
+        .build("EXO-45 Patriot Exosuit")
+}
 
 pub const fn reinforce() -> Strategem {
     Strategem::builder()
